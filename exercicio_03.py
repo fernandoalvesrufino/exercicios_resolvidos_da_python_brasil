a1331c58615e011e4e6be2de17525dc56bc63424
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-08-24 07:19:40 -0300

    Exercícios 1, 2 e 3 resolvidos

---
+++ exercicio_03.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+number_1 = float(input('Digite um número: '))
+number_2 = float(input('Digite outro número: '))
+result = number_1 + number_2
+print(f'A soma entre {number_1} e {number_2} é {result}')
