959a13d089e6c4854464e187969aaf6d9f65b956
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-08-25 05:14:35 -0300

    Adicionado resolução dos exercícios 6 e 7

---
+++ 1_estrutura_sequencial/exercicio_07.py
@@ -0,0 +1,8 @@
+# Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+largura = float(input('Informe a largura do quadrado: '))
+altura = float(input('Informe a altura do quadrado: '))
+area = largura * altura
+dobro = area * 2
+print(f'A área do quadrado é igual a {area} m2')
+print(f'O dobro da área do quadrado é igual a {dobro} m2')
