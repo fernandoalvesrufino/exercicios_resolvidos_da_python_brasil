a12d4d15f67757a07f7d6e038626d0f1e4fdd92f
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-08-25 05:23:02 -0300

    Adicionado o exercício 8

--- 1_estrutura_sequencial/exercicio_06.py
+++ 1_estrutura_sequencial/exercicio_06.py
@@ -1,5 +1,5 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 
-raio = float(input('Informe o raio do círculo: '))
-area = 3.14 * (raio**2)
-print(f'A área do circulo é igual a {area}')
+raio_circulo = float(input('Informe o raio do círculo: '))
+area_circulo = 3.14 * (raio_circulo**2)
+print(f'A área do circulo é igual a {area_circulo}')
