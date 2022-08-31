0b2cfec69ae0c8a2498b2a1987d81807f51792bc
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-08-31 06:06:24 -0300

    Adicionando resolucao do exercicio 18

---
+++ 1_estrutura_sequencial/exercicio_18.py
@@ -0,0 +1,9 @@
+# Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de
+# internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+
+tamanho = float(input('Informe o tamanho do arquivo em MB que você deseja baixar: '))
+velocidade = float(input('Informe a velocidade do link em Mbps: '))
+tempo = tamanho / (velocidade / 8)
+minutos = int(tempo / 60)
+segundos = int(tempo % 60)
+print(f'\nTempo estimado de download é de {tempo:.0f} segundos \nAproximadamente {minutos} min e {segundos} seg')
