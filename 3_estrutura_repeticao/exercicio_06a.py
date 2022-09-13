550a9a4bfde9ee5af40a5ae8f8283b54de88f579
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-09-12 21:58:15 -0300

    Create exercicio_06a.py

---
+++ 3_estrutura_repeticao/exercicio_06a.py
@@ -0,0 +1,8 @@
+# Exercício 6 - Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do 
+# outro.
+
+contador = 0
+
+while contador < 20:
+  contador += 1
+  print(contador)
