656ae659f53ddeec20914efaeb359495c413bae8
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-09-27 06:24:12 -0300

    Create exercicio_03.py

---
+++ 5_exercicios_de_funcoes/exercicio_03.py
@@ -0,0 +1,8 @@
+# Exercício 3 - Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+def soma(numero_1, numero_2, numero_3):
+  print(numero_1 + numero_2 + numero_3)
+
+
+resultado_1 = soma(1, 13, 9)
+resultado_2 = soma(14, 44, 12)
