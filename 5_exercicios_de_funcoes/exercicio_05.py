c0283420db5fb0cf7be19a345dc7dfbc1312c625
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-09-27 06:26:49 -0300

    Create exercicio_05.py

---
+++ 5_exercicios_de_funcoes/exercicio_05.py
@@ -0,0 +1,11 @@
+# Exercício 5 - Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais:
+
+# - taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem
+# - custo, que é o custo de um item antes do imposto.
+# A função “altera” o valor de custo para incluir o imposto sobre vendas.
+
+def somaImposto(taxaimposto, custo):
+  print(custo * (taxaimposto / 100))
+
+
+resultado = somaImposto(15, 2000)
