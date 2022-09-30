b4ffb4e53eff8fd553a5447a82c917e7d1b86453
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-09-30 09:25:36 -0300

    Create exercicio_14.py

---
+++ 5_exercicios_de_funcoes/exercicio_14.py
@@ -0,0 +1,85 @@
+# Exercício 14 - Quadrado mágico. Um quadrado mágico é aquele dividido em linhas e colunas, com um número em cada posição e no qual a soma das linhas, 
+# colunas e diagonais é a mesma. Por exemplo, veja um quadrado mágico de lado 3, com números de 1 a 9:
+
+# 8 - 3 - 4
+# 1 - 5 - 9
+# 6 - 7 - 2
+# Elabore uma função que identifica e mostra na tela todos os quadrados mágicos com as características acima. Dica: produza todas as combinações possíveis e 
+# verifique a soma quando completar cada quadrado. Usar um vetor de 1 a 9 parece ser mais simples que usar uma matriz 3x3.
+
+# Python program to generate
+# odd sized magic squares
+# A function to generate odd
+# sized magic squares
+
+
+def generateSquare(n):
+
+	# 2-D array with all
+	# slots set to 0
+	magicSquare = [[0 for x in range(n)]
+				for y in range(n)]
+
+	# initialize position of 1
+	i = n // 2
+	j = n - 1
+
+	# Fill the magic square
+	# by placing values
+	num = 1
+	while num <= (n * n):
+		if i == -1 and j == n: # 3rd condition
+			j = n - 2
+			i = 0
+		else:
+
+			# next number goes out of
+			# right side of square
+			if j == n:
+				j = 0
+
+			# next number goes
+			# out of upper side
+			if i < 0:
+				i = n - 1
+
+		if magicSquare[int(i)][int(j)]: # 2nd condition
+			j = j - 2
+			i = i + 1
+			continue
+		else:
+			magicSquare[int(i)][int(j)] = num
+			num = num + 1
+
+		j = j + 1
+		i = i - 1 # 1st condition
+
+	# Printing magic square
+	print("Magic Square for n =", n)
+	print("Sum of each row or column",
+		n * (n * n + 1) // 2, "\n")
+
+	for i in range(0, n):
+		for j in range(0, n):
+			print('%2d ' % (magicSquare[i][j]),
+				end='')
+
+			# To display output
+			# in matrix form
+			if j == n - 1:
+				print()
+
+# Driver Code
+
+
+# Works only when n is odd
+n = 3
+generateSquare(n)
+
+print()
+
+n = 7
+generateSquare(n)
+
+# This code is contributed
+# by Harshit Agrawal
