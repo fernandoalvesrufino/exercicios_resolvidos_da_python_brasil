39ebda115269bcaafbc21f9776e643c5268ca595
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-10-03 17:50:37 -0300

    Create exercicio_03.py

---
+++ 6_exercicios_de_strings/exercicio_03.py
@@ -0,0 +1,13 @@
+# Exercício 3 - Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+
+# F
+# U
+# L
+# A
+# N
+# O
+
+nome = input('Digite um nome: ').upper()
+
+for i in nome:
+  print(i)
