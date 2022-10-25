08541a6a90d4aea49adb899bd90d8513a1bb6edf
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-10-25 15:13:28 -0300

    Create exercicio_13.py

---
+++ 8_exercicios_de_classes/exercicio_13.py
@@ -0,0 +1,17 @@
+# Exercício 13 - Classe Funcionário: Implemente a classe Funcionário. Um empregado tem um nome (um string) e um salário(um double). Escreva um construtor 
+# com dois parâmetros (nome e salário) e métodos para devolver nome e salário. Escreva um pequeno programa que teste sua classe.
+
+class Funcionario():
+  def __init__(self, nome, salario):
+    self.nome = nome
+    self.salario = salario
+
+  def imprimirNome(self):
+    print(f'Nome do funcionário: {self.nome}')
+
+  def imprimirSalario(self):
+    print(f'O salário de {self.nome} é R$ {self.salario:.2f}')
+
+fernando = Funcionario('Fernando Rufino', 10000)
+fernando.imprimirNome()
+fernando.imprimirSalario()
